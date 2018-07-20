76231bb91b0db6b0be46085bb706d4cd61407a2b
DESKTOP-RK3FCAT\Meat <savva.ds@gmail.com>
2018-07-20 14:47:39 +0200

    BT131 Added TankAIController and Find Player logs

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -22,7 +22,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
 	virtual void BeginPlay() override;
+	ATank* GetControlledTank() const;
 
 };
